e9c066fec8e8ff3be5f9bb2375c27fa8732dde1e
Khoi7322 <nguyen.xuandangkhoi.python.edu.vn@gmail.com>
2024-07-31 18:58:32 +0700

    final deploy

--- src/commands/economy/daily.js
+++ src/commands/economy/daily.js
@@ -4,59 +4,59 @@ const User = require('../../models/User');
 const dailyAmount = 10;
 
 module.exports = {
-	name: 'daily',
-	description: 'Collect your dailies!',
-	/**
-	 *
-	 * @param {Client} client
-	 * @param {Interaction} interaction
-	 */
-	callback: async (client, interaction) => {
-		if (!interaction.inGuild()) {
-			interaction.reply({
-				content: 'You can only run this command inside a server.',
-				ephemeral: true,
-			});
-			return;
-		};
+  name: 'daily',
+  description: 'Collect your dailies!',
+  /**
+   *
+   * @param {Client} client
+   * @param {Interaction} interaction
+   */
+  callback: async (client, interaction) => {
+    if (!interaction.inGuild()) {
+      interaction.reply({
+        content: 'You can only run this command inside a server.',
+        ephemeral: true,
+      });
+      return;
+    }
 
-		try {
-			await interaction.deferReply();
+    try {
+      await interaction.deferReply();
 
-			const query = {
-				userId: interaction.member.id,
-				guildId: interaction.guild.id,
-			};
+      const query = {
+        userId: interaction.member.id,
+        guildId: interaction.guild.id,
+      };
 
-			let user = await User.findOne(query);
+      let user = await User.findOne(query);
 
-			if (user) {
-				const lastDailyDate = user.lastDaily.toDateString();
-				const currentDate = new Date().toDateString();
+      if (user) {
+        const lastDailyDate = user.lastDaily.toDateString();
+        const currentDate = new Date().toDateString();
 
-			if (lastDailyDate === currentDate) {
-				interaction.editReply(
-					'You have already collected your dailies today. Come back tomorrow!'
-				);
-				return;
-			}
-			
-			
-		} else {
-			user = new User({
-				...query,
-				lastDaily: new Date(),
-			});
-		}
+        if (lastDailyDate === currentDate) {
+          interaction.editReply(
+            'You have already collected your dailies today. Come back tomorrow!'
+          );
+          return;
+        }
+        
+        user.lastDaily = new Date();
+      } else {
+        user = new User({
+          ...query,
+          lastDaily: new Date(),
+        });
+      }
 
-		user.balance += dailyAmount;
-		await user.save();
+      user.balance += dailyAmount;
+      await user.save();
 
-		interaction.editReply(
-			`${dailyAmount}$ was added to your balance. Your new balance is: ${user.balance}$.`
-		);
-		} catch (error) {
-			console.log(`Error with /daily: ${error}`);
-		}
-	},
+      interaction.editReply(
+        `${dailyAmount}$ was added to your balance. Your new balance is ${user.balance}$.`
+      );
+    } catch (error) {
+      console.log(`Error with /daily: ${error}`);
+    }
+  },
 };
